662165110d5e0c850e5dbc9535efdf8b3bd9fe9a
Abdul <abdul9ahad@gmail.com>
2021-09-25 02:50:32 +0500

    Fixed movement of arms w.r.t mirror

--- src/index.js
+++ src/index.js
@@ -70,9 +70,7 @@ let multiplyingFactor = 5.5; // HACK: At distance of 83 inches
 let multiplyingFactorY = 1;
 let multiplyingFactorTemporary = 0.1;
 let lips;
-let leftShoulderAngleGlobal;
-let leftShoulderAngleHTML;
-let multiplierFunctionShoulders = 1;
+let multiplierFunctionShoulders = 1.5;
 // Testing variables 
 let rightAnkle, leftAnkle; // Position: 28, 27
 let rightKnee, leftKnee; // Position: 26, 25
@@ -224,7 +222,6 @@ async function animate() {
     // UIElement.innerHTML += `<h1 style="color:white">multiplier Temporary Y: ${multiplyingFactorTemporary }</h1>`;
     //  UIElement.innerHTML += `<h1 style="color:white">Hips Difference X: ${differenceHipsPosition.x }</h1>`;
     //  UIElement.innerHTML += `<h1 style="color:white">Hips Difference Y: ${differenceHipsPosition.y }</h1>`;
-      UIElement.innerHTML += `<h1 style="color:white">Angle: ${leftShoulderAngleHTML}</h1>`;
      mesh.traverse(function (child) {
       if (child.isBone) {
         let angle;
@@ -233,15 +230,14 @@ async function animate() {
           case "mixamorigLeftShoulder":
             angle = -getAngle(rightElbow, rightShoulder, 0, 0, -1);
             leftShoulderAngle = angle;
-            leftShoulderAngleHTML = angle;
             child.rotation.y = angle * multiplierFunctionShoulders ;
             // UIElement.innerHTML += `left shoulder adjusment: ${shoulderAdjustment}`;
             break;
           case "mixamorigLeftForeArm":
             angle = getAngle(rightElbow, rightWrist, 0, 0, -1);
             angle = angle - Math.PI;
-            leftShoulderAngleGlobal = (angle+leftShoulderAngle) ;
-            child.rotation.x = leftShoulderAngleGlobal * multiplierFunctionShoulders;
+            
+            child.rotation.x = (angle+leftShoulderAngle) * multiplierFunctionShoulders;
           //   // UIElement.innerHTML += `forearm angle: <b>${angle}</b><br>`;
           //   // UIElement.innerHTML += `forearm angle after adj: <b>${angle - leftShoulderAngle}</b>`;
 
@@ -365,7 +361,6 @@ window.addEventListener('keydown', (e) => {
     console.log("Mean Position", MeanPosition);
     console.log("Pivot Position:", pivot.position);
     console.log(multiplyingFactorY);
-    console.log("Left Shoulder Angle: ", leftShoulderAngleGlobal)
 
   }
   else if (e.key == "z") {
